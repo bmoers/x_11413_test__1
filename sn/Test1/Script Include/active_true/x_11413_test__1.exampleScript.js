35e6cbb0a7501565791bebc4d59efb31734cbd17
System Administrator <admin@example.com>
2019-04-23 18:44:58 +0000

    Changes by 'System Administrator' on 'Test1 – 1.0.0' -- Build #1

---
+++ sn/Test1/Script Include/active_true/x_11413_test__1.exampleScript.js
@@ -0,0 +1,28 @@
+/* 
+ * Application : Test1
+ * ClassName   : sys_script_include
+ * Created On  : 2019-04-23 17:15:52
+ * Created By  : admin
+ * Updated On  : 2019-04-23 17:15:57
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=4f9410f8db4db740fcf417803996190a
+ */
+/**
+ * Class Description
+ * 
+ * @class 
+ * @author System Administrator [admin]
+ * @memberof x_11413_test__1.module:sys_script_include
+ */
+var exampleScript = Class.create();
+exampleScript.prototype = /** @lends x_11413_test__1.module:sys_script_include.exampleScript.prototype */ {
+    /**
+     * Description
+     * 
+     * @returns {undefined}
+     */
+    initialize: function() {
+    },
+
+    type: 'exampleScript'
+};
